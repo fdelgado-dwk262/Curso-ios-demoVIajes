45abb88cefdbbf8294c3fbff29aaa2006ccb386d
Fernando <dwk262@gmail.com>
2026-01-29 10:05:57 +0100

    Initial Commit

---
+++ Curso-ios-demoVIajes/Curso_ios_demoVIajesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_demoVIajesApp.swift
+//  Curso-ios-demoVIajes
+//
+//  Created by Equipo 9 on 29/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_demoVIajesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
