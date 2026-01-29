c0286ddc7de746a3541be6ca72221b371cff0482
Fernando <dwk262@gmail.com>
2026-01-29 10:16:14 +0100

    Curso IOS Demos Viajes inicio

---
+++ Curso-ios-demoVIajes/modelos/Destino.swift
@@ -0,0 +1,16 @@
+//
+//  Destino.swift
+//  Curso-ios-demoVIajes
+//
+//  Created by Equipo 9 on 29/1/26.
+//
+
+import Foundation
+
+struct Destino: Identifiable, Hashable {
+    var id: String
+    var nombre: String
+    var pais: String
+    var descripcion: String
+    var imagen: String
+}
